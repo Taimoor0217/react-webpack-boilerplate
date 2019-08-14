d4366a8751db9165701021493717b9dc792758aa
taimoor0217 <20100217@lums.edu.pk>
2019-08-14 07:58:01 +0500

    Add basic config and set dev and build scripts

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import App from './App'
+import  React from "react";
+import  ReactDOM from "react-dom";
+import App from "./Components/App.jsx";
 ReactDOM.render(
-    <App/> , 
-    document.getElementById('root')
-)
+    <App/>,
+    document.getElementById("app"),
+);
--- webpack.common.js
+++ webpack.common.js
@@ -1,16 +1,27 @@
 const path = require('path')
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry: {
-        main: './src/index.js' //for non static user wtitten code
+        main: path.join(__dirname, "src/index.jsx") //for non static user wtitten code
     },
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist")
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            use: ['babel-loader']
-        }]
-    }
+        rules: [
+            {
+                test :/\jsx?$/,
+                exclude : /node_modules/,
+                use : {
+                    loader : "babel-loader"
+                }
+            }
+        ]
+    },
+    plugins :[
+        new HTMLWebpackPlugin({
+            template : path.join(__dirname, "src/index.html")
+        })
+    ],
 }
